e596b26220ae4a8fd5ee7a947cfa651a08aa7404
Alexander <flo0.webmaster@gmail.com>
2019-01-11 16:23:33 +0700

    5c373d194147ac437b428f7b Добавить Request к интерфейсу модуля

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -5,5 +5,6 @@ A set of tools to develop web scrapers!
 from .crawler import Crawler, StealthCrawler
 from .proxy import ProxyManager
 from .scraper import Scraper
+from .downloader import Request
 
 from .api import crawl
